2e5f26abaea445447c92db1782eacfebfe982d9c
Hyacinth <hyacinthcomrade@outlook.com>
2020-07-28 00:47:27 +0800

    添加billboard作为单例添加进游戏； 添加billboard中提供的四则运算方法，以及大小判断方法； 添加director的values字典作为切换场景时保留的局部变量

--- class/happy_director.gd
+++ class/happy_director.gd
@@ -6,3 +6,4 @@ export(Dictionary) var storys : Dictionary
 export(Dictionary) var coordinate : Dictionary
 export(Array) var tools : Array
 export(Vector2) var editor_offset : Vector2
+export(Dictionary) var values : Dictionary
